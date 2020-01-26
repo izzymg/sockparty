3ac73b268817bd2f6bb6c899fa677824bd9da73b
izzymg <izzymhg@gmail.com>
2020-01-26 18:31:34 +1300

    Refactored bully test HTTP server

--- bully_test.go
+++ bully_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"testing"
 	"time"
 
@@ -14,10 +15,38 @@ import (
 	"nhooyr.io/websocket"
 )
 
-func spawnrw(messageCount int, got chan int) func() {
+func getAddress() string {
+	if addr, ok := os.LookupEnv("TEST_ADDR"); ok {
+		return addr
+	}
+	return "localhost:3500"
+}
 
+// Simple HTTP server to serve parties
+func basicPartyServer(party *sockparty.Party, stop chan bool) {
+	server := http.Server{
+		Addr:    getAddress(),
+		Handler: party,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	select {
+	case <-stop:
+		server.Shutdown(context.Background())
+	}
+}
+
+/* Spawn a websocket connection and send n messages, writing back to 'got'
+each time a message is receieved */
+func spawnrw(messageCount int, got chan int) func() {
+	addr := fmt.Sprintf("ws://%s", getAddress())
 	// Create a connection
-	conn, _, err := websocket.Dial(context.Background(), "ws://localhost:3500", nil)
+	conn, _, err := websocket.Dial(context.Background(), addr, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -25,7 +54,6 @@ func spawnrw(messageCount int, got chan int) func() {
 	// Write n messages to it
 	go func() {
 		for i := 0; i < messageCount; i++ {
-			<-time.After(time.Millisecond * 10)
 			err := conn.Write(context.Background(), websocket.MessageText, []byte(`{ "event": "message", "data": "bah" }`))
 			if err != nil {
 				panic(err)
@@ -45,16 +73,60 @@ func spawnrw(messageCount int, got chan int) func() {
 		}
 	}()
 
+	// Return cleanup function
 	return func() {
 		conn.Close(websocket.StatusNormalClosure, "Going away")
 	}
 }
 
-// Bully an echo server
-func TestBully(t *testing.T) {
+// Spawn n conns
+func bully(connCount int, messagesPerConn int) int {
+	got := make(chan int)
+	messagesReceived := 0
 
-	connectionCount := 10
-	messagesPerConnection := 200
+	// Spawn n websocket connection loops
+	for i := 0; i < connCount; i++ {
+		cleanup := spawnrw(messagesPerConn, got)
+		defer cleanup()
+	}
+
+	// Start incrementing connections received
+	for {
+		select {
+		case <-got:
+			messagesReceived++
+			if messagesReceived == connCount*messagesPerConn {
+				return messagesReceived
+			}
+		}
+	}
+}
+
+// Bully an active server
+func BenchmarkRealBully(b *testing.B) {
+	connCount := 3
+	messagesPerConn := 100
+
+	got := make(chan int)
+	// Spawn n websocket connection loops
+	for i := 0; i < connCount; i++ {
+		cleanup := spawnrw(messagesPerConn, got)
+		defer cleanup()
+	}
+
+	// Start incrementing connections received
+	for {
+		select {
+		case <-got:
+			continue
+		case <-time.After(time.Second * 30):
+			return
+		}
+	}
+}
+
+// Bully an echo server
+func TestEchoBully(t *testing.T) {
 
 	// Create party, can't ping as client doesn't implement pong
 	party := sockparty.NewParty("", &sockparty.Options{
@@ -70,46 +142,20 @@ func TestBully(t *testing.T) {
 		panic(fmt.Errorf("Test: Invalid message"))
 	}
 
+	// Spawn server
+	stop := make(chan bool)
+	go basicPartyServer(party, stop)
 	go party.Listen()
 	defer func() {
 		party.StopListening <- true
+		stop <- true
 	}()
 
-	// Create HTTP server
-	server := http.Server{
-		Addr:    "localhost:3500",
-		Handler: party,
-	}
-	defer server.Shutdown(context.Background())
-
-	go func() {
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
-
-	// Setup connection vars
-	got := make(chan int, connectionCount)
-	messagesReceived := 0
-
-	for i := 0; i < connectionCount; i++ {
-		cleanup := spawnrw(messagesPerConnection, got)
-		defer cleanup()
-	}
-
-	// Start incrementing connections received
-	going := true
-	for going {
-		select {
-		case <-got:
-			messagesReceived++
-			if messagesReceived == connectionCount*messagesPerConnection {
-				going = false
-			}
-		}
-	}
+	connCount := 10
+	messagesPerConn := 200
+	messagesReceived := bully(connCount, messagesPerConn)
 	// Assert that got as many messages as connections were echoed back
-	if messagesReceived != connectionCount*messagesPerConnection {
-		log.Fatalf("Expected to recieve %d messages but got %d", connectionCount*messagesPerConnection, messagesReceived)
+	if messagesReceived != connCount*messagesPerConn {
+		log.Fatalf("Expected to recieve %d messages but got %d", connCount*messagesPerConn, messagesReceived)
 	}
 }
